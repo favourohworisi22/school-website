640fa3759645479e7a9b833fe10ac5873c1de621
favourohworisi22 <72106784+favourohworisi22@users.noreply.github.com>
2026-01-05 01:06:25 -0800

    Update primary.php

--- primary.php
+++ primary.php
@@ -1,1 +1,20 @@
+<?php include "includes/header.php"; ?>
 
+<div class="page-banner">
+  <img src="assets/images/banner 8.jpg">
+</div>
+
+<section class="container">
+<h2>Primary School</h2>
+<p>
+Our Primary School curriculum is designed to strengthen literacy, numeracy,
+critical thinking, and creativity while promoting discipline and leadership.
+</p>
+
+<p>
+Students are guided by qualified teachers using modern instructional tools
+to ensure holistic development.
+</p>
+</section>
+
+<?php include "includes/footer.php"; ?>
