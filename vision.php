4731acaa51d17fe39aece1a31d5ddc11a32472be
favourohworisi22 <72106784+favourohworisi22@users.noreply.github.com>
2026-01-05 01:09:47 -0800

    Update vision.php

--- vision.php
+++ vision.php
@@ -1,1 +1,20 @@
+<?php include "includes/header.php"; ?>
+
+<div class="page-banner">
+     <img src="assets/images/banner 7.jpg">
+   
+<section class="container my-5">
+  <h2>Our Vision</h2>
+  <p>
+    Our vision is to be a leading international school recognized for excellence
+    in academics, discipline, innovation, and character development.
+  </p>
+
+  <p>
+    We aim to raise future leaders who are intellectually sound, socially
+    responsible, spiritually guided, and globally competitive.
+  </p>
+</section>
+
+<?php include "includes/footer.php"; ?>
 
