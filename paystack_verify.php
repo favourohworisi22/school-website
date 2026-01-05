1369e7075e0e9b0879c84c8b25ec342ba813bb77
favourohworisi22 <72106784+favourohworisi22@users.noreply.github.com>
2026-01-05 01:03:10 -0800

    Update paystack_verify.php

--- paystack_verify.php
+++ paystack_verify.php
@@ -1,1 +1,9 @@
+<?php
+include "config/config.php";
+
+$ref = uniqid();
+$email = $_POST['email'];
+$amount = $_POST['amount'] * 100;
+
+header("Location: https://api.paystack.co/transaction/initialize");
 
