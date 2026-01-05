ea67e4685335225c2bcf45b185e384d470a88636
favourohworisi22 <72106784+favourohworisi22@users.noreply.github.com>
2026-01-05 01:07:41 -0800

    Update secondary.php

--- secondary.php
+++ secondary.php
@@ -1,1 +1,24 @@
+<?php include "includes/header.php"; ?>
+
+<div class="page-banner">
+  <img src="assets/images/banner 8.jpg">
+</div>
+
+<section class="container">
+<h2>Secondary School</h2>
+<p>
+Our Secondary School prepares students for national and international
+examinations through rigorous academics, ICT integration, and career guidance.
+</p>
+
+<p>
+We emphasize leadership, responsibility, and excellence to prepare students
+for higher education and future careers.
+</p>
+</section>
+
+<?php include "includes/footer.php"; ?>
+
+
+
 
