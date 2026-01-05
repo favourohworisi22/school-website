95ca855d1d40bef87e91740df8d60b97f827e57c
favourohworisi22 <72106784+favourohworisi22@users.noreply.github.com>
2026-01-05 00:12:09 -0800

    Update mission.php

--- mission.php
+++ mission.php
@@ -1,1 +1,24 @@
+<?php include "includes/header.php"; ?>
+
+<div class="page-banner">
+  <img src="assets/images/banner 10.jpg">
+</div>
+
+<section class="container">
+<h2>Our Mission</h2>
+<p>
+Our mission at Victory Favour International Schools is to provide a balanced,
+innovative, and child-centered education that nurtures intellectual excellence,
+moral uprightness, leadership capacity, and global relevance.
+</p>
+
+<p>
+We are committed to creating a safe, inclusive, and stimulating learning
+environment where every child is inspired to discover their purpose,
+develop confidence, and achieve academic success through modern teaching
+methods and strong ethical values.
+</p>
+</section>
+
+<?php include "includes/footer.php"; ?>
 
