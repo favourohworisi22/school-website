91852132b932380b1fdea6bbd5cf31b92d1047fa
favourohworisi22 <72106784+favourohworisi22@users.noreply.github.com>
2026-01-05 00:00:27 -0800

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Victory Favour International Schools</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark school-header">
+  <div class="container">
+    <a class="navbar-brand fw-bold" href="index.php">Victory Favour Schools</a>
+
+    <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#menu">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="menu">
+      <ul class="navbar-nav ms-auto">
+
+        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown">About</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="about.php">About Us</a></li>
+            <li><a class="dropdown-item" href="mission.php">Mission</a></li>
+            <li><a class="dropdown-item" href="vision.php">Vision</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Academics</a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="nursery.php">Nursery</a></li>
+            <li><a class="dropdown-item" href="primary.php">Primary</a></li>
+            <li><a class="dropdown-item" href="secondary.php">Secondary</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item"><a class="nav-link" href="admissions.php">Admissions</a></li>
+        <li class="nav-item"><a class="nav-link" href="payment.php">Pay Fees</a></li>
+        <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
